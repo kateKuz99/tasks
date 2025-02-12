7f56432fce673c824714d16c6a3f13d098e66cc9
kateKuz99 <30katya2003@gmail.com>
2025-02-12 18:22:14 +0300

    add 2 tasks

---
+++ src/main/java/org/example/TwoSum.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>(); // Хранение числа и его индекса
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        int[] result1 = twoSum(new int[]{2, 7, 11, 15}, 9);
+        System.out.println("[" + result1[0] + ", " + result1[1] + "]"); // [0, 1]
+
+        int[] result2 = twoSum(new int[]{3, 2, 4}, 6);
+        System.out.println("[" + result2[0] + ", " + result2[1] + "]"); // [1, 2]
+
+        int[] result3 = twoSum(new int[]{3, 3}, 6);
+        System.out.println("[" + result3[0] + ", " + result3[1] + "]"); // [0, 1]
+    }
+}
